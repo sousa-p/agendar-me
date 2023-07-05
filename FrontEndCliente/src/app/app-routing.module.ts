7637f602ae8ec8095e43e4404192e874803e53b6
sousa-p <p.soousa7@gmail.com>
2023-07-05 16:03:42 -0300

    Adicionando estruturas html

--- FrontEndCliente/src/app/app-routing.module.ts
+++ FrontEndCliente/src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'cadastrar',
+    loadChildren: () => import('./pages/cadastrar/cadastrar.module').then( m => m.CadastrarPageModule)
+  }
 ];
 
 @NgModule({
---
+++ FrontEndCliente/src/app/pages/cadastrar/cadastrar.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar',
+  templateUrl: './cadastrar.page.html',
+  styleUrls: ['./cadastrar.page.scss'],
+})
+export class CadastrarPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
