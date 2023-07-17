f2b71ec578092e680515a03456b59261512049bf
sousa-p <p.soousa7@gmail.com>
2023-07-17 17:55:42 -0300

    #

--- FrontEndCliente/src/app/pages/home/home-routing.module.ts
+++ FrontEndCliente/src/app/pages/home/home-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomePage } from './home.page';
+
 const routes: Routes = [
   {
     path: '',
