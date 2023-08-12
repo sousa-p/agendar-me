29e849137ac18ded83d8a13f10015a795b74ceb5
sousa-p <p.soousa7@gmail.com>
2023-08-12 18:00:08 -0300

    Feature pesquisa de clientes

--- FrontEndComercio/src/app/pages/clientes/clientes.page.ts
+++ FrontEndComercio/src/app/pages/clientes/clientes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 import { User } from 'src/app/core/interface/User';
 import { ComercioService } from 'src/app/core/service/comercio.service';
 
@@ -8,22 +9,28 @@ import { ComercioService } from 'src/app/core/service/comercio.service';
   styleUrls: ['./clientes.page.scss'],
 })
 export class ClientesPage implements OnInit {
-
-  constructor(private Comercio: ComercioService) { }
+  constructor(private Comercio: ComercioService) {}
 
   ngOnInit() {
     this.Comercio.getClientes().subscribe(
       (response) => {
         this.clientes = response;
+        this.clientesFiltrados = response;
+
+        this.mostrarItensClientes();
       },
-      (error) =>  {
+      (error) => {
         console.error(error);
       }
-    )
+    );
   }
 
-  clientes: User[] = [];
+  clientes?: User[];
+  clientesFiltrados: User[] = [];
+  clientesPagina: User[] = [];
+
   clienteSelecionado?: User;
+  clienteAtual: number = 0;
 
   isModalOpen: boolean = false;
 
@@ -34,5 +41,28 @@ export class ClientesPage implements OnInit {
 
   setModalOpen(open: boolean) {
     this.isModalOpen = open;
+  }
+
+  carregarClientesPagina(ev: any) {
+    this.mostrarItensClientes();
+    setTimeout(() => {
+      (ev as InfiniteScrollCustomEvent).target.complete();
+    }, 500);
+  }
+
+  mostrarItensClientes() {
+    this.clienteAtual += 15;
+    this.clientesPagina = this.clientesFiltrados?.slice(0, this.clienteAtual);
+  }
+
+  filtrar(event: any) {
+    const pesquisa = event.target.value.toLowerCase();
+    this.clientesFiltrados = this.clientes!.filter(
+      (d) => {
+        return d.NOME_USER.toLocaleLowerCase().startsWith(pesquisa.trim().toLowerCase());
+      }
+    );
+    this.clienteAtual = 0;
+    this.mostrarItensClientes();
   }
 }
